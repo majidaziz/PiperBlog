762a54c3605613f65ba94d9a0c9c79a8b7cff524
casta145 <casta145@mail.chapman.edu>
2018-12-12 15:29:08 -0800

    Update v.2

--- app/src/main/java/com/example/cuadmin/testblog/LoginActivity.java
+++ app/src/main/java/com/example/cuadmin/testblog/LoginActivity.java
@@ -103,7 +103,7 @@ public class LoginActivity extends AppCompatActivity
                     loginEmailText.setText("");
                     loginPassText.setText("");
                 }
-                
+
             }
         });
     }
